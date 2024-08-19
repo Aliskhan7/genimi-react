581fb321e600fee6628e7c3f03997b0bc7bda9e6
aliskhangazamatov <ag@farcana.com>
2024-08-19 20:27:46 +0300

    create main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Sidebar from "./components/Siderbar/Sidebar.jsx";
+import Main from "./components/Main/Main.jsx";
 
 function App() {
   return (
     <>
       <Sidebar />
+      <Main />
     </>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Compass, User } from "@phosphor-icons/react";
+import "./Main.css";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <User className="user" size={40} />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Dev</span>
+          </p>
+          <Compass size={30} />
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest beautiful place to see on an upcoming road trip.</p>
+            <Compass size={30} />
+          </div>
+          <div className="card">
+            <p>Suggest beautiful place to see on an upcoming road trip.</p>
+            <Compass size={30} />
+          </div>
+          <div className="card">
+            <p>Suggest beautiful place to see on an upcoming road trip.</p>
+            <Compass size={30} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
