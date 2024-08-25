917f11a3171d13b58bf1ee1ba639916fab7ad0c2
aliskhangazamatov <ag@farcana.com>
2024-08-25 19:29:07 +0300

    add result prompt

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,6 +8,7 @@ import {
   User,
   Image,
   Microphone,
+  OpenAiLogo,
 } from "@phosphor-icons/react";
 import "./Main.css";
 import { Context } from "../../context/Context.jsx";
@@ -36,24 +37,47 @@ const Main = () => {
           </p>
           <p>How can I help you today?</p>
         </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest beautiful place to see on an upcoming road trip.</p>
-            <Compass className="cars-icon" size={35} />
+        {!showResult ? (
+          <>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest beautiful place to see on an upcoming road trip.</p>
+                <Compass className="cars-icon" size={35} />
+              </div>
+              <div className="card">
+                <p>Briefly summarize this concept: urban planning</p>
+                <Lightbulb className="cars-icon" size={35} />
+              </div>
+              <div className="card">
+                <p>Brainstorm team bonding activities for our work retreat</p>
+                <ChatDots className="cars-icon" size={35} />
+              </div>
+              <div className="card">
+                <p>Tell me about React js and React native.</p>
+                <Code className="cars-icon" size={35} />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <User size={20} />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <OpenAiLogo size={32} weight="thin" />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
           </div>
-          <div className="card">
-            <p>Briefly summarize this concept: urban planning</p>
-            <Lightbulb className="cars-icon" size={35} />
-          </div>
-          <div className="card">
-            <p>Brainstorm team bonding activities for our work retreat</p>
-            <ChatDots className="cars-icon" size={35} />
-          </div>
-          <div className="card">
-            <p>Tell me about React js and React native.</p>
-            <Code className="cars-icon" size={35} />
-          </div>
-        </div>
+        )}
 
         <div className="main-bottom">
           <div className="search-box">
